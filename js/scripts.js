bd8b5e553b1d10ccb39352d54278ee42f5154510
Guy <luckythelich@hotmail.com>
2017-05-12 10:08:28 -0700

    this will add 3 starter toppings, and allow the page to transition to the next form.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,17 @@ function showHide(show, hide,){
 
 
 $(document).ready(function(){
+
   $("form#size").submit(function(event) {
   event.preventDefault();
   });
+
+  $("form#sauce").submit(function(event) {
+  event.preventDefault();
+  });
+
+  $("form#topping").submit(function(event) {
+  event.preventDefault();
+  });
+
 });
