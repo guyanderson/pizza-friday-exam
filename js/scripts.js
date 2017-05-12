85b20d7a7a0e1159fc76832f957fab1e4e428109
Guy <luckythelich@hotmail.com>
2017-05-12 09:45:48 -0700

    this will switch between size and sauce display.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ function showHide(show, hide,){
    $(show).show();
    $(hide).hide();
 }
+
+
+
+
+
+
+$(document).ready(function(){
+  $("form#size").submit(function(event) {
+  event.preventDefault();
+  });
+});
