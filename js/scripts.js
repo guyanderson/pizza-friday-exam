798ed740a01293c0e32257cbe42795c55486f3d0
Guy <luckythelich@hotmail.com>
2017-05-12 14:36:36 -0700

    turned buttons into spans to stop early submit errors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
+function Order(inputPizzaSize, inputSauce) {
+  this.pizzaSize = inputPizzaSize;
+  this.pizzaSauce = inputSauce;
+  this.pizzaTopping = toppingArray;
+  console.log(this.pizzaTopping);
+}
+
 function showHide(show, hide,){
    $(show).show();
    $(hide).hide();
 }
-
-
 
 
 
@@ -13,12 +18,17 @@ $(document).ready(function(){
 
   $("form#pizzaOrder").submit(function(event) {
     event.preventDefault();
+
     var inputPizzaSize = $("input:radio[name=pizzaSize]:checked").val();
     var inputSauce = $("input:radio[name=sauceType]:checked").val();
-    var input = $("input:radio[name=top1]:checked").val();
-    var inputPizzaSize = $("input:radio[name=top2]:checked").val();
-    var inputPizzaSize = $("input:radio[name=top3]:checked").val();
+    var toppingArray = [];
+
+    $("input:checkbox[name=topping]:checked").each(function(){
+      var inputTopping = $(this).val();
+      toppingArray.push(inputTopping);
+      newOrder.pizzaTopping = toppingArray;
+ // console.log(toppingArray);
     });
-
-
+    var newOrder = new Order(inputPizzaSize, inputSauce, pizzaTopping);
+  });
 });
