a93321418259562706fe256397de3852938db39b
guy <luckythelich@hotmail.com>
2017-05-15 17:39:05 -0700

    THis will finally get toppings pushed into the topping array of the order constructor. Pizza size and Pizza sauce will be appended to the screen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-function Order(inputPizzaSize, inputSauce) {
+function Order(inputPizzaSize, inputSauce, toppingArray) {
   this.pizzaSize = inputPizzaSize;
   this.pizzaSauce = inputSauce;
   this.pizzaTopping = toppingArray;
   console.log(this.pizzaTopping);
 }
 
-function showHide(show, hide,){
+function showHide(show, hide){
    $(show).show();
    $(hide).hide();
 }
@@ -22,13 +22,15 @@ $(document).ready(function(){
     var inputPizzaSize = $("input:radio[name=pizzaSize]:checked").val();
     var inputSauce = $("input:radio[name=sauceType]:checked").val();
     var toppingArray = [];
+    // debugger
 
     $("input:checkbox[name=topping]:checked").each(function(){
       var inputTopping = $(this).val();
       toppingArray.push(inputTopping);
-      newOrder.pizzaTopping = toppingArray;
  // console.log(toppingArray);
     });
-    var newOrder = new Order(inputPizzaSize, inputSauce, pizzaTopping);
+    var newOrder = new Order(inputPizzaSize, inputSauce, toppingArray);
+    $("#pizza-size").text(newOrder.pizzaSize);
+    $("#pizza-sauce").text(newOrder.pizzaSauce);
   });
 });
